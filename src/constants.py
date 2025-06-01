4ab8101a2daa19b2f74bcfde74cda8791bbf75cc
Ross Yeager <ryeager12@gmail.com>
2025-06-01 16:02:09 -0700

    updated constatns

--- src/constants.py
+++ src/constants.py
@@ -9,8 +9,6 @@ load_dotenv()
 
 ROOT_DIR = Path(__file__).parent.parent
 CSV_DIR = ROOT_DIR / "csv"
-CSV_TEMPLATE = (
-    CSV_DIR / f"follow_up_boss_people_{datetime.now().strftime('%Y-%m-%d')}.csv"
-)
+CSV_TEMPLATE = CSV_DIR / f"follow_up_boss_people_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.csv"
 
 API_KEY = os.getenv("API_KEY", "")
